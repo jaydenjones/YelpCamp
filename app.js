1a2f28f4a897ab8f62ab8d3d61ee8138cba4ed00
Jayden Jones <jmjone46@gmail.com>
2020-07-11 20:24:50 +0000

    initial deploy

---
+++ app.js
@@ -0,0 +1,53 @@
+var express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser"),
+    mongoose      = require("mongoose"),
+	Campground    = require("./models/campground"),
+	Comment       = require("./models/comment"),
+	flash 		  = require("connect-flash");
+	seedDB        = require("./seeds"),
+	passport      = require("passport"),
+	localStrategy = require("passport-local"),
+	User          = require("./models/user"),
+	methodOverride =  require("method-override");
+
+//requiring routes	
+var campgroundRoutes = require("./routes/campgrounds"),
+	commentRoutes    = require("./routes/comments"),
+	indexRoutes      = require("./routes/index");
+	 
+
+mongoose.connect("mongodb://localhost/yelp_camp_v12", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+//seedDB(); //seed the database
+
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({	 
+	secret: "ONCE AGAIN, FRAN IS THE CUTEST DOG!",
+	resave: false,
+	saveUninitialized: false	
+}));
+app.use(passport.initialize());
+app.use(passport.session())
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+app.listen(3000, function(){
+	console.log("YelpCamp server has started!");
+});
