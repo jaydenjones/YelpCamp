0781fb9620e8397f6ddc9b154557bc9595796d30
jaydenjones <44034298+jaydenjones@users.noreply.github.com>
2020-07-12 19:42:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -61,10 +61,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-// app.listen(3000, function(){
-// 	console.log("YelpCamp server has started!");
-// });
-
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("YelpCamp server has started!");
 });
